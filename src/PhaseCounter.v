4b93b847c60816133e4f75e885ea2fa9028ae384
SnoozeLee <likegongcheng@foxmail.com>
2022-12-18 10:43:44 +0800

    [fix] 修复占空比低电平过低的问题

--- src/PhaseCounter.v
+++ src/PhaseCounter.v
@@ -7,16 +7,18 @@ module PhaseCounter(
     output [7:0] phase     //输出相位
 );
 // 在此次设计中，2pai分解为256个点
-reg [7:0] phase_state;    // 当前相位状态
+reg [8:0] phase_state;    // 当前相位状态
 
 always @(posedge clk or negedge en) begin
     if(!en) begin phase_state = init_phase; end
     else if(clk) begin 
-        phase_state = phase_state + step; // 设置了定宽[7:0]，自动达到取模的效果
+        // phase_state = phase_state + step; // 设置了定宽[7:0]，自动达到取模的效果 -> 但是这样会导致信号抖动（因为每次刷新周期时可能都是不同相位)
+        phase_state = phase_state + step; 
+        if(phase_state >= 9'b100000000) begin phase_state = init_phase; end
     end
     else begin phase_state = init_phase; end
 end
 
-assign phase = phase_state;
+assign phase = phase_state[7:0];
 
 endmodule   // PhaseCounter
